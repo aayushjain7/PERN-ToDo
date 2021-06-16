25fa9a91206c7af1f4cdf05b036ef306fa747449
Aayush Jain <jaayush014@gmail.com>
2021-06-16 17:16:43 +0530

    Build input todo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import InputTodo from './components/InputTodo';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<div className='container'>
+				<InputTodo />
+			</div>
+		</>
+	);
 }
 
 export default App;
