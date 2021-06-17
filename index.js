465e6bb4d397e6840b91643f9c1f20ca986f0350
Aayush Jain <jaayush014@gmail.com>
2021-06-17 17:20:30 +0530

    update server index.js

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 app.post('/todos', async (req, res) => {
 	try {
@@ -56,6 +63,6 @@ app.delete('/todos/:id', async (req, res) => {
 	}
 });
 
-app.listen(5000, () => {
-	console.log('Server running on port 5000');
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
